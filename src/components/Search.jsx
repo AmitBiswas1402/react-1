9ff345e129a2bfe03584e8b6b0d227ff457a7aae
AmitBiswas1402 <amit.142biswas@gmail.com>
2025-09-06 21:31:54 +0530

    feat: add search component and implement movie fetching logic

---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+const Search = ({ searchTerm, setSearchTerm }) => {
+  return (
+    <div className="search">
+      <div>
+        <img src="search.svg" alt="" />
+
+        <input
+          type="text"
+          placeholder="Search through all movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        
+        
+
+      </div>
+    </div>
+  );
+};
+export default Search;
